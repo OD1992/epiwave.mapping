1872f17da96d7d0fce48b21dd114ba4e30ad86fb
Nick Golding <goldingn35@gmail.com>
2024-12-18 13:50:44 +0800

    add spatial-only BCB GP code

--- R/define_bcb_setup.R
+++ R/define_bcb_setup.R
@@ -3,29 +3,83 @@
 #' .. content for \details{} ..
 #'
 #' @title
-#' @param grid_raster
+#' @param grid_raster a terra spatRaster object for which to create a
+#'   block-circulant basis representation. This should have a reasonable
+#'   projected CRS, so that cells can be assumed to be in a regular grid on a
+#'   two-dimensional plane
+#' @param check_lonlat whether to error if the user provides a raster
+#'   that appears to have a longitude/latitude (and therefore unprojected) CRS,
+#'   as tested by `terra::is.lonlat()`
 #' @return
 #' @author Nick Golding
 #' @export
-define_bcb_setup <- function(grid_raster) {
+define_bcb_setup <- function(grid_raster, check_lonlat = TRUE) {
 
-  stop("TO DO")
+  # check this raster doesn't look unprojected
+  looks_lonlat <- terra::is.lonlat(grid_raster)
+  if (looks_lonlat & check_lonlat) {
+    stop("grid_raster appears to have a longitude/latitude CRS rather than a
+         projected CRS. Please apply a reasonable projection, or set
+         check_lonlat = FALSE if this is incorrect")
+  }
 
-  # check this raster is projected
+  # stash the original, to preserve
+  original_raster <- grid_raster
 
-  # get the dimensions of the raster
+  # remove NA space around the edges
+  grid_raster <- trim(grid_raster)
 
-  # double them to get the toroidal dimensions
+  # get the dimensions
+  res <- res(grid_raster)
+  m <- ncol(grid_raster)
+  n <- nrow(grid_raster)
 
-  # build the BCB objects: basis, number of elements
+  # create an expanded grid (representing a mapping to locations on the surface
+  # of a torus) that has dimensions that are a power of 2, and are at least
+  # twice as big in each dimension as the original grid. This ensures that
+  # simulation of the GP is exact (given the planar regular grid assumption),
+  # since the minimum distance between two points is across the plain (within
+  # the original grid), rather than around the back of the torus.
+  M <- ceiling2(2 * m)
+  N <- ceiling2(2 * n)
 
-  # get an index to the cells we care about in the torus
+  # get the distances from each cell to the centre of the extended torus grid
+  x_sq <- centred_squared_distance(M, res[1])
+  y_sq <- centred_squared_distance(N, res[2])
+  dist_squared <- outer(x_sq, y_sq, FUN = "+")
+  dist <- sqrt(dist_squared)
 
-  # return a list of these things
-  list(
-    n = basis_size,
-    basis = basis,
-    dim = basis_dimensions,
-    original_dim = original_dimensions)
+  # represent the centre location in the spectral domain
+  adjust <- matrix(0, nrow = M, ncol = N)
+  adjust[M / 2, N / 2] <- 1
+  fft_adjust <- (stats::fft(adjust) * M * N)
 
+  # find the locations of the non-missing cells in the original raster
+  # not_missing_idx <- which(!is.na(values(grid_raster)))
+
+  # find their locations in the extended torus grid (using the top left corner)
+  extract_coords <- cbind(rowFromCell(grid_raster, cells(grid_raster)),
+                          colFromCell(grid_raster, cells(grid_raster)))
+
+  # return this info as a list
+  list(dim_all = c(M, N),
+       dim_sub = c(m, n),
+       dist = dist,
+       fft_adjust = fft_adjust,
+       extract_coords = extract_coords,
+       grid_raster = grid_raster)
+
+}
+
+# find the smallest integer, larger than `n`, that is an integer-power of 2
+ceiling2 <- function(n) {
+  2 ^ ceiling(log2(n))
+}
+
+# given a number of cells `n`, and a cell width `diff`, return a vector giving
+# the squared distance from the centrepoint of the vector
+centred_squared_distance <- function(n, diff) {
+  seq <- seq_len(n) * diff
+  mid <- diff * n / 2
+  (seq - mid) ^ 2
 }
--- R/sim_bcb_gp.R
+++ R/sim_bcb_gp.R
@@ -19,9 +19,17 @@ sim_bcb_gp <- function(bcb_setup = bcb_setup,
 
   # given the bcb setup and number of timesteps to simulate define the latent
   # white noise parameter
-  z <- normal(0, 1, dim = c(bcb_setup$n, n_times))
+  v <- normal(0, 1, dim = bcb_setup$dim_all)
 
-  # evaluate kernel on the bcb space
+  # evaluate kernel on the bcb space and get simulated values for all non-NA
+  # pixels in the template raster
+  f <- bcb_gp(v = v,
+              kernel = space_kernel,
+              bcb_setup = bcb_setup)
+
+  # work out how to efficiently do this in a vectorised way, to get multiple draws
+
+
   stop("TO DO")
 
   # apply the bcb fft step
@@ -49,3 +57,136 @@ sim_bcb_gp <- function(bcb_setup = bcb_setup,
 
 
 }
+
+
+
+bcb_gp_colour <- function (bcb_setup, v_all, kernel) {
+
+  # apply the greta.gp spatial kernel to the minimal vector of distances (one
+  # per gridcell on the torus, so ~4x the number of gridcells in a square region
+  # of interest) representing the BCB setup, to get the corresponding
+  # covariances
+
+  # since the kernel is expecting locations, but the bcb_setup provides
+  # distances from the centroid, we need to flatten the grid of distances,
+  # compute covariance from 0, and then reshape
+  dist_vec <- as.vector(bcb_setup$dist)
+  covar <- kernel(dist_vec, 0)
+  dim(covar) <- dim(bcb_setup$dist)
+
+  # if v_all is a list, loop through applying it on the pre-evaluated kernel
+  if (is.list(v_all) && inherits(v_all[[1]], "greta_array")) {
+
+    ans <- lapply(v_all,
+                  function (v) {
+                    spectral_colour(covar, v, bcb_setup)
+                  })
+
+    # otherwise just apply the 'colouring' once
+  } else {
+
+    ans <- spectral_colour(covar, v_all, bcb_setup)
+
+  }
+
+  ans
+
+}
+
+# given two greta arrays, with matching dimensions, for pointwise covariances
+# and standard normal random variables, 'colour' them (apply
+# correlation/covariance) and return the greta array of coloured random
+# variables
+spectral_colour <- function (covar, v, bcb_setup) {
+
+  # check inputs are both greta arrays
+  if (!inherits(covar, "greta_array") |
+      !inherits(v, "greta_array")) {
+    stop ("'covar' and 'v' must both be greta arrays",
+          call. = FALSE)
+  }
+
+  # check they have the same dimensions
+  if (!identical(dim(covar), dim(v))) {
+    stop ("'covar' and 'v' must have the same dimensions, ",
+          "but 'covar' had dimensions ",
+          paste0(dim(covar), collapse = "x"),
+          " and 'v' had dimensions ",
+          paste0(dim(v), collapse = "x"),
+          call. = FALSE)
+  }
+
+  # create and return an operation greta array for the result
+  op <- greta::.internals$nodes$constructors$op
+
+  op("spectral_colour",
+     covar, v,
+     operation_args = list(bcb_setup = bcb_setup),
+     tf_operation = "tf_spectral_colour")
+
+}
+
+# tensorflow code for the spectral colouring operation
+tf_spectral_colour <- function (covar, v, bcb_setup) {
+
+  tf <- tensorflow::tf
+
+  original_dim <- dim(covar)
+  vec_shape <- tensorflow::shape(prod(original_dim))
+
+  # cast these to complex (only type fft accepts)
+  complex_type <- tf$complex128
+  covar <- tf$cast(covar, complex_type)
+  covar_vec <- tf$reshape(covar, vec_shape)
+  v <- tf$cast(v, complex_type)
+  v_vec <- tf$reshape(v, vec_shape)
+
+  # cast covariance and colourless normals into spectral domain
+  fft_adjust_vec <- tf$cast(as.vector(t(bcb_setup$fft_adjust)),
+                            complex_type)
+
+  covar_fft <- tf$signal$fft(covar_vec) / fft_adjust_vec
+  v_fft <- tf$signal$fft(v_vec)
+
+  # get coloured function in spectral domain - vectorise this?!
+  f_fft <- tf$math$sqrt(covar_fft) * v_fft
+
+  # bring coloured function back from the spectral domain
+  f <- tf$math$real(tf$signal$ifft(f_fft))
+
+  # rescale
+  root_nelem <- sqrt(prod(dim(v)))
+  f <- f / root_nelem
+
+  # cast the float type back, reshape and return
+  f <- tf$cast(f, options()$greta_tf_float)
+  f <- tf$reshape(f, original_dim)
+  f
+
+}
+
+# given the bcb setup information and the full coloured random variables,
+# extract only those the user wants
+bcb_gp_extract <- function (bcb_setup, z) {
+
+  # vectorisation by recursion(!)
+  if (is.list(z) && inherits(z[[1]], "greta_array")) {
+
+    ans <- lapply(z, function (z) bcb_gp_extract(bcb_setup, z))
+
+  } else {
+
+    ans <- z[bcb_setup$extract_coords]
+
+  }
+
+  ans
+
+}
+
+bcb_gp <- function (v, kernel, bcb_setup) {
+  z_all <- bcb_gp_colour(bcb_setup, v, kernel)
+  z <- bcb_gp_extract(bcb_setup, z_all)
+  z
+}
+
