32afc295d0584903c4e9b1dd20467957a537a990
Nick Golding <goldingn35@gmail.com>
2024-12-18 15:42:58 +0800

    fix permutation (fft acts on final dimension)

--- R/sim_bcb_gp.R
+++ R/sim_bcb_gp.R
@@ -139,11 +139,10 @@ tf_spectral_colour <- function (covar_vec, v, bcb_setup) {
 
   tf <- tensorflow::tf
 
-  # load the fft adjustment vector, and reshape to align with batch dimensions
-  # etc.
+  # load the fft adjustment vector, and reshape to align with dimensions
+  # of other arrays (needed to avoid a crash at complex division step!)
   fft_adjust_vec <- bcb_setup$fft_adjust_vec
   dim(fft_adjust_vec) <- dim(covar_vec)
-  # fft_adjust_vec <- greta:::expand_to_batch(fft_adjust_vec, covar_vec)
 
   # find the complex type corresponding to the user's float type
   float_type <- options()$greta_tf_float
@@ -157,25 +156,14 @@ tf_spectral_colour <- function (covar_vec, v, bcb_setup) {
   v <- tf$cast(v, complex_type)
   fft_adjust_vec <- tf$cast(fft_adjust_vec, complex_type)
 
-  # TF's fft operates on the innermost dimension of the tensor, so move the
-  # batch dimension around to do calculations
-  perm <- c(1L, 2L, 0L)
-  reverse_perm <- c(2L, 0L, 1L)
+  # TF's fft operates on the innermost (last) dimension of the tensor, but we
+  # want it on the second dimension, so move batch dimensions around to do
+  # calculations
+  perm <- c(0L, 2L, 1L)
+  reverse_perm <- c(0L, 2L, 1L)
   covar_vec <- tf$transpose(covar_vec, perm)
   v <- tf$transpose(v, perm)
   fft_adjust_vec <- tf$transpose(fft_adjust_vec, perm)
-
-  # original_dim <- dim(covar)
-  # vec_shape <- tensorflow::shape(prod(original_dim))
-
-  # covar_vec <- tf$reshape(covar, vec_shape)
-  # v_vec <- tf$reshape(v, vec_shape)
-
-  # correctly shape the fixed adjustment, including tiling across batch
-  # dimensions
-  # fft_adjust_vec <- tf$cast(as.vector(t(bcb_setup$fft_adjust)),
-  #                           complex_type)
-  # fft_adjust_vec <- tf$reshape(fft_adjust_vec, dim(covar_vec))
 
   # cast covariance and colourless normals into spectral domain
   covar_vec_fft <- tf$signal$fft(covar_vec) / fft_adjust_vec
@@ -188,17 +176,14 @@ tf_spectral_colour <- function (covar_vec, v, bcb_setup) {
   f <- tf$math$real(tf$signal$ifft(f_fft))
 
   # rescale by the dimension size
-  # warning("change to nrow when vectorised")
   root_nelem <- greta:::fl(sqrt(dim(v)[1]))
   f <- f / root_nelem
 
   # cast the float type back
   f <- tf$cast(f, options()$greta_tf_float)
 
-  # permute it again, to put the batch dimension first, and return
-
+  # permute it again, to put the spatial dimension second again, and return
   f <- tf$transpose(f, reverse_perm)
-  # f <- tf$reshape(f, original_dim)
   f
 
 }
@@ -214,11 +199,7 @@ bcb_gp_extract <- function (bcb_setup, z) {
 
   } else {
 
-    # convert back from transposed TF version
-    dim(z) <- rev(bcb_setup$dim_all)
-    z <- t(z)
-    ans <- z[bcb_setup$extract_coords]
-    # ans <- z[bcb_setup$extract_index_vec]
+    ans <- z[bcb_setup$extract_index_vec]
 
   }
 
